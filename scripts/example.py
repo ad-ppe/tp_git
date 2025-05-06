6503a010bf150054dbd25f2bb508883a342239a4
ad-ppe <adriendebreuilly.ppe@gmail.com>
2025-05-06 14:34:14 +0200

    bidibidibou

--- scripts/example.py
+++ scripts/example.py
@@ -2,6 +2,7 @@
 
 def hello_world():
     print("Bonjour à tous depuis une feature !")
+    print("va te faire ... ") #je te laisse remplir le reste ou modifier a toi de voir
 
 if __name__ == "__main__":
     hello_world()
