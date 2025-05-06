a68105861c3628153746e592e699785dbb99e302
ad-ppe <adriendebreuilly.ppe@gmail.com>
2025-05-06 14:50:20 +0200

    test0

--- scripts/example.py
+++ scripts/example.py
@@ -3,6 +3,7 @@
 def hello_world():
     print("Bonjour à tous depuis une feature !")
     print("va te faire ... ") #je te laisse remplir le reste ou modifier a toi de voir
+    print("test")
 
 if __name__ == "__main__":
     hello_world()
